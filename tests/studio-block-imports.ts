cb13b3863357a7cfa51bdd27b851e88924c41468
sionxai <nshcarlue@gmail.com>
2026-05-18 08:38:33 +0900

    test(studio): 분리된 component import smoke test

---
+++ tests/studio-block-imports.ts
@@ -0,0 +1,7 @@
+import { ImagePreviewModal } from "@/components/studio/blocks/image-preview-modal";
+import { StudioWorkspaceHeader } from "@/components/studio/blocks/studio-workspace-header";
+
+export const studioBlockImports = [
+  ImagePreviewModal,
+  StudioWorkspaceHeader
+] as const;
